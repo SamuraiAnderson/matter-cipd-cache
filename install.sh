624db1e889b0f5c7d464cb8f52b7469b5d1e69bc
SamuraiAnderson <pangurbango@gmail.com>
2025-12-17 00:57:29 +0800

    add pigweed_environment.gni file

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # 解压 .environment 到项目目录
 cd /path/to/connectedhomeip
-tar -xzvf matter-environment.tar.gz
+
+
+tar -xzvf matter-environment.tar.gz -C 
 
 # 解压 .cipd-cache-dir 到用户主目录
 tar -xzvf matter-cipd-cache.tar.gz -C ~
+
